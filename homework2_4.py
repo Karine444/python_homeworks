d6ece5973d6b4aa3f2fd3de3fefe302d552ad800
Karine <karine.vardanyan.444@gmail.com>
2021-10-28 15:08:23 +0400

    Creates a new json file and populates it with the values obtained from the User class

--- homework2_4.py
+++ homework2_4.py
@@ -1,11 +1,10 @@
 import json
+# import yaml
 
 class User:
-    def __init__(self, name, surname, email, phone_number):
+    def __init__(self, name, surname):
         self.name = self.string_validation(name)
         self.surname = self.string_validation(surname)
-        self.email = self.email_validator(email)
-        self.phone_number = self.number_validation(phone_number)
 
     @staticmethod
     def string_validation(value):
@@ -14,25 +13,46 @@ class User:
         else:
             return value
 
-    @staticmethod
-    def number_validation(value):
-        if type(value) == str:
-            raise ValueError("Wrong Value")
-        else:
-            return value
-
-    @staticmethod
-    def email_validator(self, email):
-        if "@" not in email:
-            raise ValueError("Wrong type of email!!")
-        return email
-
     def __str__(self):
-        return f"{self.name} {self.surname} {self.email} {self.phone_number}"
+        return f"{self.name} {self.surname}"
 
-    def to_dict(self):
-        return{"name":self.name, "surname":self.surname, "email":self.email, "phone_number":self.phone_number}
+    def to_json(self):
+        return{"name":self.name, "surname":self.surname}
 
 
-user_1 = User('Karine', 'Vardanyan', 'karine.vardanyan.444@mail.ru', '094758489')
-user_2 = User('Anna', 'Simonyan', 'anna.simonyan.444@mail.ru', '098452515')
+class MyJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, User):
+            return o.to_json()
+        else:
+            super().default(o)
+
+try:
+    with open("users.json", 'x') as fd:
+        json.dump([], fd, indent=4)
+except FileExistsError as err:
+    print("file already exists: passing")
+
+if __name__ == "__main__":
+
+    user_1 = User('Karine', 'Vardanyan')
+    user_2 = User('Vardan', 'Simonyan')
+
+    user_list = [user_1, user_2]
+
+
+def insert_user_to_json(user_as_dict):
+
+    with open("users.json", 'r') as json_file:
+            users = json.load(json_file)
+
+    users.append(user_as_dict)
+    with open('users.json', 'w') as json_file:
+        json.dump(users, json_file, indent=4, cls=MyJSONEncoder)
+
+
+
+for user in user_list:
+    insert_user_to_json(user)
+
+
